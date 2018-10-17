8d412d856dd8c7bff0ca8a48a963091e7c83572c
Jhonny <jhonnypaulino16@gmail.com>
2018-10-16 22:17:31 -0500

    agregue opcion de foto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera : Camera) {
+
+  }
+
+  tomarFoto() {
+
+  	const options : CameraOptions = {
+  		destinationType : this.camera.DestinationType.DATA_URL,
+  		encondigType : this.camera.EncodigType.JPEG,
+  		mediaType : this.camera.MediaType.PICTURE
+  	}
+  	this.camera.getPicture(options).then((imageData) => {
+	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+		console.log(err)
+	});
 
   }
 
