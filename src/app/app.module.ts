c9078015ad35273752a85cdf6c60d6e611b3b860
Jhonny Paulino <jhonnypaulino16@egmail.com>
2018-11-13 22:14:26 -0500

    Apliquee Contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule} from '@angular/http';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -23,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+  HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +9,24 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+
+	public data : any;
+  constructor(
+  public navCtrl: NavController,
+	public http: Http
+
+  ) {
+
+  	this.listarPosts();
 
   }
+  	listarPosts(){
+	  	this.http.get("http://cesar.pe/wp-json/wp/v2/posts")
+	  	.map(res=>res.json())
+	  	.subscribe(data => {
+	  	this.data= data;
+	  	console.log(data);
 
+  	})
+  }
 }
